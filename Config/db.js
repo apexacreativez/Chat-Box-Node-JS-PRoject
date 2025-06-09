686d2a106ec2ca2575e6df631eed787925ebe9e1
apexacreativez <apexacreativez@gmail.com>
2025-06-09 16:23:00 +0530

    Db.js change file

--- Config/db.js
+++ Config/db.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql2/promise');
 
 const mySqlPool = mysql.createPool({
-  
-     host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME,
-    port     : process.env.DB_PORT || 8085 
-})
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_NAME,
+  port     : process.env.DB_PORT || 26266,
+  ssl      : {
+    rejectUnauthorized: true
+  }
+});
+
 
 module.exports = mySqlPool;
